1cb4f724fdbf560e96feadb02dbbc49a176b0c2f
Seth Pellegrino <seth@codecopse.net>
2023-03-18 09:38:51 -0700

    monads!

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,72 @@
-use std::fmt::Debug;
-use std::ops::Deref;
+use std::{fmt::Debug, vec};
 
-use nom;
 use nom::bytes::streaming::tag;
-use nom::{character, IResult};
+use nom::IResult;
 
 fn main() {
     println!("Hello, world!");
 
     #[derive(Debug)]
     struct I;
-    impl IntoExtensionShape for I {
-        fn into_shape(&self) -> ExtensionShape {
-            ExtensionShape::Tag(String::from("I"))
-        }
-
-        fn generate(&self) -> Vec<Extension> {
-            vec![Extension(String::from("I"))]
-        }
+    impl SingleCharShape for I {
+        const NAME: char = 'I';
     }
-    println!("{:?}", parse_one("I", Box::new(I)));
+    println!("{:?}", parse_one("I", &I));
 
     #[derive(Debug)]
+    struct M;
+    impl SingleCharShape for M {
+        const NAME: char = 'M';
+    }
+    #[derive(Debug)]
+    struct A;
+    impl SingleCharShape for A {
+        const NAME: char = 'A';
+    }
+    #[derive(Debug)]
+    struct C;
+    impl SingleCharShape for C {
+        const NAME: char = 'C';
+    }
+
+    #[derive(Debug, Default)]
     struct X(String);
     impl IntoExtensionShape for X {
-        fn into_shape(&self) -> ExtensionShape {
+        fn as_shape(&self) -> ExtensionShape {
             ExtensionShape::Prefix(String::from("X"))
-        }
-
-        fn generate(&self) -> Vec<Extension> {
-            vec![Extension(format!("X{}", self.0))]
         }
     }
 
-    println!("{:?}", parse_one("Xabcd", Box::new(X(Default::default()))));
+    println!("{:?}", parse_one("Xabcd", &X::default()));
 
     #[derive(Debug)]
     struct G;
     impl IntoExtensionShape for G {
-        fn into_shape(&self) -> ExtensionShape {
+        fn as_shape(&self) -> ExtensionShape {
             ExtensionShape::Multi(String::from("G"))
         }
         fn generate(&self) -> Vec<Extension> {
-            ["i", "m", "a", "c"].into_iter().map(|x| Extension(x.to_string())).collect()
+            ["i", "m", "a", "c"]
+                .into_iter()
+                .map(|x| Extension(x.to_string()))
+                .collect()
         }
     }
-    
-    println!("{:?}", parse_one("G", Box::new(G)));
+
+    println!("{:?}", parse_one("G", &G));
+    println!("{:?}", parse_one("q", &G));
+
+    println!(
+        "{:?}",
+        parse("IMACXmyext", vec![&I, &M, &A, &C, &X::default()])
+    );
 }
 
 #[derive(Debug)]
 pub enum ExtensionShape {
     Tag(String),
     Prefix(String),
-    Multi(String)
+    Multi(String),
 }
 
 impl ExtensionShape {
@@ -66,27 +79,42 @@ impl ExtensionShape {
     }
 }
 
-pub trait IntoExtensionShape {
-    fn into_shape(&self) -> ExtensionShape;
+pub trait SingleCharShape {
+    const NAME: char;
+}
+impl<T: SingleCharShape> IntoExtensionShape for T {
+    fn as_shape(&self) -> ExtensionShape {
+        ExtensionShape::Tag(String::from(Self::NAME))
+    }
+}
 
-    fn generate(&self) -> Vec<Extension>;
+pub trait IntoExtensionShape {
+    fn as_shape(&self) -> ExtensionShape;
+
+    fn generate(&self) -> Vec<Extension> {
+        unimplemented!()
+    }
 }
 
 #[derive(Debug)]
 pub struct Extension(String);
 
-pub fn parse_one(input: &str, ext: Box<dyn IntoExtensionShape>) -> IResult<&str, Vec<Extension>> {
-    let shape = ext.into_shape();
+pub fn parse_one<'str>(
+    input: &'str str,
+    ext: &dyn IntoExtensionShape,
+) -> IResult<&'str str, Vec<Extension>> {
+    let shape = ext.as_shape();
     match shape {
         ExtensionShape::Tag(_) => {
             let id = shape.identifier();
-            nom::bytes::complete::tag(id)(input).map(|(rest, x)| (rest, vec![Extension(x.to_owned())]))
+            nom::bytes::complete::tag(id)(input)
+                .map(|(rest, x)| (rest, vec![Extension(x.to_owned())]))
         }
         ExtensionShape::Prefix(_) => {
             let id = shape.identifier();
             nom::sequence::pair(tag(id), nom::bytes::complete::take_till1(|c| c == '_'))(input)
                 .map(|(rest, (id, tail))| (rest, vec![Extension(format!("{}{}", id, tail))]))
-        },
+        }
         ExtensionShape::Multi(_) => {
             let id = shape.identifier();
             nom::bytes::complete::tag(id)(input).map(|(rest, _)| (rest, ext.generate()))
@@ -94,9 +122,25 @@ pub fn parse_one(input: &str, ext: Box<dyn IntoExtensionShape>) -> IResult<&str,
     }
 }
 
-pub fn parse(input: &str, extensions: Vec<Box<dyn IntoExtensionShape>>) -> IResult<&str, Vec<Extension>> {
-    for ext in extensions {
-        let parsed = parse_one(input, ext);
+pub fn parse<'str>(
+    input: &'str str,
+    extensions: Vec<&dyn IntoExtensionShape>,
+) -> IResult<&'str str, Vec<Extension>> {
+    let mut res = vec![];
+    let mut remaining = input;
+    while !remaining.is_empty() {
+        let parsed;
+        (remaining, parsed) = extensions
+            .as_slice()
+            .iter()
+            .find_map(|&ext| parse_one(remaining, ext).ok())
+            .ok_or(nom::Err::Failure(nom::error::Error {
+                input: remaining,
+                code: nom::error::ErrorKind::OneOf, // or something?
+            }))?;
+
+        res.extend(parsed);
     }
-    Ok(("", vec![]))
+
+    Ok(("", res))
 }
